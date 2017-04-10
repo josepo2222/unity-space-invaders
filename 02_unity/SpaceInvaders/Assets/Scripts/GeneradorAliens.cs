733953465534e7bc4358cc37b7b0b4814326b1c1
josepo2222 <josepo2000@gmail.com>
2017-04-10 22:35:39 +0200

    cambios varios

--- 02_unity/SpaceInvaders/Assets/Scripts/GeneradorAliens.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/GeneradorAliens.cs
@@ -7,22 +7,23 @@ public class GeneradorAliens : MonoBehaviour
 
 	// Publicamos la variable para conectarla desde el editor
 	public Rigidbody2D prefabAlien1;
+    public Rigidbody2D prefabnalien;
 
 	// Referencia para guardar una matriz de objetos
 	private Rigidbody2D[,] aliens;
-
+    
 	// Tamaño de la invasión alienígena
 	private const int FILAS = 4;
 	private const int COLUMNAS = 7;
 
 	// Enumeración para expresar el sentido del movimiento
 	private enum direccion { IZQ, DER };
-
-	// Rumbo que lleva el pack de aliens
-	private direccion rumbo = direccion.DER;
-
-	// Posición vertical de la horda (lo iremos restando de la .y de cada alien)
-	private float altura = 0.5f;
+    
+    // Rumbo que lleva el pack de aliens
+    private direccion rumbo = direccion.DER;
+    
+    // Posición vertical de la horda (lo iremos restando de la .y de cada alien)
+    private float altura = 0.5f;
 
 	// Límites de la pantalla
 	private float limiteIzq;
@@ -44,76 +45,93 @@ public class GeneradorAliens : MonoBehaviour
 		limiteIzq = -1.0f * distanciaHorizontal + 1;
 		limiteDer = 1.0f * distanciaHorizontal - 1;
 	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		// Contador para saber si hemos terminado
-		int numAliens = 0;
 
-		// Variable para saber si al menos un alien ha llegado al borde
-		bool limiteAlcanzado = false;
+    // Update is called once per frame
+    void Update()
+    {
+        // Contador para saber si hemos terminado
+        int numAliens = 0;
+        
+        // Variable para saber si al menos un alien ha llegado al borde
+        bool limiteAlcanzado = false;
+        
+        // Recorremos la horda alienígena
+        for (int i = 0; i < FILAS; i++)
+        {
+            for (int j = 0; j < COLUMNAS; j++)
+            {
 
-		// Recorremos la horda alienígena
-		for (int i = 0; i < FILAS; i++) {
-			for (int j = 0; j < COLUMNAS; j++) {
+                // Comprobamos que haya objeto, para cuando nos empiecen a disparar
+                if (aliens[i, j] != null)
+                {
 
-				// Comprobamos que haya objeto, para cuando nos empiecen a disparar
-				if (aliens [i, j] != null) {
+                    // Un alien más
+                    numAliens += 1;
 
-					// Un alien más
-					numAliens += 1;
+                    // ¿Vamos a izquierda o derecha?
+                    if (rumbo == direccion.DER)
+                    {
 
-					// ¿Vamos a izquierda o derecha?
-					if (rumbo == direccion.DER) {
+                        // Nos movemos a la derecha (todos los aliens que queden)
+                        aliens[i, j].transform.Translate(Vector2.right * velocidad * Time.deltaTime);
 
-						// Nos movemos a la derecha (todos los aliens que queden)
-						aliens [i, j].transform.Translate (Vector2.right * velocidad * Time.deltaTime);
+                        // Comprobamos si hemos tocado el borde
+                        if (aliens[i, j].transform.position.x > limiteDer)
+                        {
+                            limiteAlcanzado = true;
+                        }
+                    }
+                    else
+                    {
 
-						// Comprobamos si hemos tocado el borde
-						if (aliens [i, j].transform.position.x > limiteDer) {
-							limiteAlcanzado = true;
-						}
-					} else {
+                        // Nos movemos a la derecha (todos los aliens que queden)
+                        aliens[i, j].transform.Translate(Vector2.left * velocidad * Time.deltaTime);
 
-						// Nos movemos a la derecha (todos los aliens que queden)
-						aliens [i, j].transform.Translate (Vector2.left * velocidad * Time.deltaTime);
+                        // Comprobamos si hemos tocado el borde udemi
+                        if (aliens[i, j].transform.position.x < limiteIzq)
+                        {
+                            limiteAlcanzado = true;
+                        }
+                    }
+                }
+            }
+        }
 
-						// Comprobamos si hemos tocado el borde udemi
-						if (aliens [i, j].transform.position.x < limiteIzq) {
-							limiteAlcanzado = true;
-						}
-					}		
-				}
-			}
-		}
+        // Si no quedan aliens, hemos terminado
+        if (numAliens == 0 /*&& numnaliens == 0*/)
+        {
+            SceneManager.LoadScene("Nivel1");
+        }
 
-		// Si no quedan aliens, hemos terminado
-		if( numAliens == 0 ) {
-			SceneManager.LoadScene ("Nivel1");
-		}
+        // Si al menos un alien ha tocado el borde, todo el pack cambia de rumbo
+        if (limiteAlcanzado == true)
+        {
+            for (int i = 0; i < FILAS; i++)
+            {
+                for (int j = 0; j < COLUMNAS; j++)
+                {
 
-		// Si al menos un alien ha tocado el borde, todo el pack cambia de rumbo
-		if (limiteAlcanzado == true) {
-			for (int i = 0; i < FILAS; i++) {
-				for (int j = 0; j < COLUMNAS; j++) {
-
-					// Comprobamos que haya objeto, para cuando nos empiecen a disparar
-					if (aliens [i, j] != null) {
-						aliens[i,j].transform.Translate (Vector2.down * altura);
-					}
-				}
-			}
+                    // Comprobamos que haya objeto, para cuando nos empiecen a disparar
+                    if (aliens[i, j] != null)
+                    {
+                        aliens[i, j].transform.Translate(Vector2.down * altura);
+                    }
+                }
+            }
 
 
-			if (rumbo == direccion.DER) {
-				rumbo = direccion.IZQ;
-			} else {
-				rumbo = direccion.DER;
-			}
-		}
-	}
+            if (rumbo == direccion.DER)
+            {
+                rumbo = direccion.IZQ;
+            }
+            else
+            {
+                rumbo = direccion.DER;
+            }
+        }
 
+
+    }
 	void generarAliens (int filas, int columnas, float espacioH, float espacioV, float escala = 1.0f)
 	{
 		/* Creamos una rejilla de aliens a partir del punto de origen
@@ -147,7 +165,9 @@ public class GeneradorAliens : MonoBehaviour
 				alien.transform.localScale = new Vector2 (0.2f * escala, 0.2f * escala);
 			}
 		}
-
-	}
+      /*  Vector2 posicion = new Vector2(0 + (espacioH * j), 3 + (espacioV * i));
+        Rigidbody2D alien = (Rigidbody2D)Instantiate(prefabnalien, posicion, transform.rotation);
+        */
+    }
 
 }
